ad25dd63d150ce2418336125d28bac724879c881
RootOfMinus1ATU <s00240122@atu.ie>
2024-03-03 22:31:27 +0000

    Update main.rs

--- cleverbot-bot/src/main.rs
+++ cleverbot-bot/src/main.rs
@@ -7,6 +7,7 @@ mod utils {
 
 mod cleverbot;
 use cleverbot::CleverbotConversation;
+use regex::Error;
 use serde_json::json;
 
 use std::collections::HashMap;
@@ -15,6 +16,11 @@ use std::vec;
 use reqwest::header::COOKIE;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use reqwest::blocking;
+
+use chrono::prelude::*;
+use regex::Regex;
+
 
 #[derive(Debug)]
 #[derive(Deserialize, Serialize)]
@@ -22,75 +28,131 @@ struct ChuckNorrisJoke {
     value: String,
 }
 
-fn get_clevereq_path() -> PathBuf {
-    let mut adaptable_path = PathBuf::from(".");
-    adaptable_path.push("src");
 
-    if cfg!(windows) {
-        adaptable_path.push("clevreq.exe");
-    } else {
-        adaptable_path.push("clevreq");
-    };
 
-    adaptable_path
+
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+pub struct MaxQueue<T> {
+    max: usize,
+    list: VecDeque<T>,
 }
 
-fn get_cleverbot_response() -> Result<String, String> {
-    // Define the command to run the Python script
-    let python_exe = get_clevereq_path(); // Replace with the actual path
-    let cookie = "";
-    let payload = "";
-
-    // Execute the Python script as a subprocess
-    let output = Command::new(python_exe)
-        .arg("--cookie")
-        .arg(cookie)
-        .arg("--payload")
-        .arg(payload)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .output()
-        .expect("Failed to execute command");
-
-    // Check if the Python script executed successfully
-    if output.status.success() {
-        // Capture and print the output of the Python script
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        return Ok(stdout.to_string());
-    } else {
-        // Capture and print any error messages
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        return Err(stderr.to_string());
+impl<T> MaxQueue<T> {
+    pub fn new(max: usize) -> MaxQueue<T> {
+        MaxQueue {
+            max: max,
+            list: VecDeque::with_capacity(max),
+        }
     }
 
+    pub fn push_front(&mut self, item: T) {
+        if self.list.len() == self.max {
+            self.list.pop_back();
+        }
+        self.list.push_front(item);
+    }
 
+    pub fn push_back(&mut self, item: T) {
+        if self.list.len() == self.max {
+            self.list.pop_front();
+        }
+        self.list.push_back(item);
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.list.pop_front()
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.list.pop_back()
+    }
+
+    pub fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.list.clear();
+    }
+
+    pub fn get_all(&self) -> &VecDeque<T> {
+        &self.list
+    }
 }
 
-use std::path::{Path, PathBuf};
+
+#[derive(Debug)]
+pub struct Cleverbot {
+    pub api_link: String,
+    pub api_key: String,
+    pub cookie: String,
+    pub context: MaxQueue<String>,
+}
+
+impl Cleverbot {
+    pub fn new(api_key: String, api_link: String, cookie: String, max: usize) -> Self {
+        Cleverbot {
+            api_link: api_link,
+            api_key: api_key,
+            cookie: cookie,
+            context: MaxQueue::new(max),
+        }
+    }
+
+    fn update_context(&mut self, stimulus: &str, response: &str) {
+        self.context.push_front(stimulus.to_string());
+        self.context.push_front(response.to_string());
+    }
+
+    async fn get_response(&mut self, stimulus: &str) -> Result<String, Box<dyn std::error::Error>> {
+        let req_params = serde_json::to_string(&json!({
+            "stimulus": stimulus,
+            "context": self.context.get_all(),
+        }))?;
+
+        let response = Client::new()
+            .post(&self.api_link)
+            .header("cookie", &self.cookie)
+            .header("clevreq-api-key", &self.api_key)
+            .body(req_params)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let cleared_response = response.trim_matches('"');
+
+        self.update_context(stimulus, cleared_response);
+    Ok(response)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // time to use paths and pthbufs to get to the python executable
-    // remember that linux and windows are different
+    
+    let mut cleverbot = Cleverbot::new(
+        "key".to_string(),
+        "no".to_string(),
+        "nuh".to_string(),
+        10,
+    );
 
-    // let response = get_cleverbot_response()?;
-    // println!("response is: {}", response);
-    let api_key = "lol";
-    let cookie = "hi";
-    let api_str = "";
-        
-    let res = Client::new()
-        .post(api_str)
-        .header("cookie", cookie)
-        .header("clevreq-api-key", api_key)
-        .body(serde_json::to_string(&json!({
-            "stimulus": "nah",
-            "context": vec![ "do you wanna build a snowman?", "hi" ],
-        }))?)
-        .send()
-        .await?;
+    let res = cleverbot.get_response("Are you a bot?").await?;
 
-    println!("{}", res.text().await?);
+    println!("Got response: {}", res);
+
+    
+    let res = cleverbot.get_response("you are wrong...").await?;
+
+    println!("Got response: {}", res);
+
+    println!("{:#?}", cleverbot);
 
 
     Ok(())
